4f3fed6e40d5118bb780b0591704e3d39ae64102
Thanh Phuc <phucsecond6554@gmail.com>
2017-08-18 16:02:45 +0700

    Them link phan goi y tim kiem

--- js/search.js
+++ js/search.js
@@ -1,21 +1,26 @@
+var base_url = "http://localhost/CI_Blog/";
+var template = "<a></a>";
+
 function ajax_search(search_key){
   $.ajax({
-    url: 'http://localhost/CI_Blog/Search/search_suggest',
+    url: base_url + "Search/search_suggest",
     type: 'get',
     dataType: 'json',
     data:{
       'search_key' : search_key
     },
     success: function(result){
-      var html = "";
       $.each(result,function(key,item){
-        html += "<a href='#'>"+item['title'] + "</a>";
+        var test = $(template);
+
+        test.html(item['title']);
+        test.attr("href", base_url + "Post/" + item['url']);
+
+        $("#search-suggest").append(test);
       });
 
-      $("#search-suggest").css('display','block');
-      $("#search-suggest").html(html);
-
-      console.log(html);
+      $("#search-suggest").css("display", "block");
+      console.log("Success");
     }
   });
 }
@@ -25,8 +30,18 @@ function search_suggest(){
   //console.log(search_key);
 
   if(search_key != ""){
+    $("#search-suggest").html("");
     ajax_search(search_key);
   }else{
     $("#search-suggest").css('display','none');
   }
-}
+};
+
+
+$(document).ready(function(){
+  $(window).click(function(e){
+    if(e.target != $("#search-suggest")){
+      $("#search-suggest").css('display','none');
+    }
+  });
+})
